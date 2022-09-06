257a877ea64149e075647a1457167ab88a4699fc
nurulamin092 <mnurulamin092@gmial.com>
2022-09-06 12:24:18 +0600

    comparison operator

---
+++ comparison.js
@@ -0,0 +1,38 @@
+/**
+ * == equal to (দুইটাই সমান )
+ * != equal to (সমান না )
+ * < smaller than / less then (একটা আরেক থেকে ছোট )
+ * > gater than (বড় )
+ * <= (একটা আরেকটা থেকে ছোট ও হতে পারে আবার সমান  হতে পারে )
+ * >= (একটা আরেকটা থেকে বড়  ও হতে পারে আবার সমান  হতে পারে)
+ */
+
+/* let n = 17;
+
+if (n < 10) {
+    console.log(n, 'this number', n, 'is smaller than 10');
+} else if (n < 20) {
+    console.log(n, 'this number', n, 'is smaller than 20');
+}
+
+else {
+    console.log(n, 'this number', n, 'is greater than 10');
+} */
+
+// var n = 21;
+// var m = 22;
+
+// if (n >= m) {
+//     console.log('n is greater than m');
+// } else {
+//     console.log('n is smaller than m');
+// }
+
+let n = 10;
+
+if (8 == n) {
+    console.log('n is equal to 8');
+}
+else {
+    console.log('n is not equal to 8');
+}
