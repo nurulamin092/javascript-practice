2d0255638d1b8ed4b4e6b68561be407a24ee4401
nurulamin092 <mnurulamin092@gmial.com>
2022-09-06 15:52:15 +0600

    negative and positive

--- comparison.js
+++ comparison.js
@@ -28,11 +28,53 @@ else {
 //     console.log('n is smaller than m');
 // }
 
-let n = 10;
+/* let n = 10;
 
 if (8 == n) {
     console.log('n is equal to 8');
 }
 else {
     console.log('n is not equal to 8');
+} */
+
+
+/* 
+
+let personOne = 'jamal';
+let personTwo = 'kamal';
+let thereAreBrother = true;
+
+if ('jamal' == personOne) {
+    if ('kamal' == personTwo) {
+        if (thereAreBrother) {
+            console.log('Jamal & Kamal Brother');
+        } else {
+            console.log('there are not brother');
+        }
+    }
+}
+
+if ('jamal' == personOne && 'kamal' == personTwo && thereAreBrother) {
+    console.log('Jamal & Kamal Brother');
+}
+else {
+    console.log('there are not brother');
+} 
+ */
+
+
+
+// 2 3,5,7
+
+let n = 18;
+
+if (n < 10 && (2 == n || 3 == n || 5 == n || 7 == n)) {
+    console.log("This number", n, "is smaller than 10 & it's a prime number ");
+}
+else if (n < 10) {
+    console.log("This number", n, "is smaller than 10 & it's not a prime number");
+}
+
+else {
+    console.log("This number", n, "is greater than 10");
 }
---
+++ negative-or-positive.js
@@ -0,0 +1,11 @@
+var n = - 12;
+
+if (n > 0) {
+    var result = "This number" + n + "is positive";
+}
+else {
+    // var result = `This number"  ${n}  "is negative`;
+    var result = "This number" + n + "is negative";
+}
+
+console.log(result);
